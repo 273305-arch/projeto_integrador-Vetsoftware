51027512e9561a3834875671c89f9ab9cf66ebec
273305-arch <273305@unifio.edu.br>
2026-09-03 23:58:01 +0000

    exercicio 2

--- pi/src/app/listadeexercicio/listadeexercicio-module.ts
+++ pi/src/app/listadeexercicio/listadeexercicio-module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { ListadeexercicioRoutingModule } from './listadeexercicio-routing-module';
 import { Exercicio1 } from './exercicio1/exercicio1';
+import { Exercicio2 } from './exercicio2/exercicio2';
 
 @NgModule({
-  declarations: [Exercicio1],
-  imports: [CommonModule, ListadeexercicioRoutingModule, FormsModule] ,
+  declarations: [Exercicio1, Exercicio2],
+  imports: [CommonModule, ListadeexercicioRoutingModule, FormsModule],
 })
 export class ListadeexercicioModule {}
